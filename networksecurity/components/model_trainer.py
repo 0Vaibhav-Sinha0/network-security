bd8064d9f5cb567db974a7ebc0ee8415dbd8285f
Your Name <vaibhavsinha082000@gmail.com>
2026-04-22 14:14:22 +0530

    ML Flow Tracking

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -29,6 +29,9 @@ from sklearn.ensemble import (
     RandomForestClassifier,
 )
 
+import mlflow
+from urllib.parse import urlparse
+import dagshub
 
 class ModelTrainer:
     """Handles model training, evaluation, and saving the final model."""
@@ -129,8 +132,8 @@ class ModelTrainer:
         )
 
         # MLflow 
-        # self.track_mlflow(best_model,classification_train_metric)
-        # self.track_mlflow(best_model,classification_test_metric)
+        self.track_mlflow(best_model,classification_train_metric)
+        self.track_mlflow(best_model,classification_test_metric)
 
 
         # =========================
@@ -197,3 +200,29 @@ class ModelTrainer:
 
         except Exception as e:
             raise NetworkSecurityException(e, sys)
+        
+
+    def track_mlflow(self,best_model,classificationmetric):
+        # mlflow.set_registry_uri("https://dagshub.com/krishnaik06/networksecurity.mlflow")
+        # tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+        with mlflow.start_run():
+            f1_score=classificationmetric.f1_score
+            precision_score=classificationmetric.precision_score
+            recall_score=classificationmetric.recall_score
+
+            
+
+            mlflow.log_metric("f1_score",f1_score)
+            mlflow.log_metric("precision",precision_score)
+            mlflow.log_metric("recall_score",recall_score)
+            mlflow.sklearn.log_model(best_model,"model")
+            # Model registry does not work with file store
+            # if tracking_url_type_store != "file":
+
+                # Register the model
+                # There are other ways to use the Model Registry, which depends on the use case,
+                # please refer to the doc for more information:
+                # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+            #     mlflow.sklearn.log_model(best_model, "model", registered_model_name=best_model)
+            # else:
+            #     mlflow.sklearn.log_model(best_model, "model")
